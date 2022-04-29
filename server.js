761b8600addf2aaf043c9ff713ea8453b0155afd
unknown <idokokennethnduka@gmail.com>
2022-04-29 09:32:51 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const server = express();
+const port = process.env.PORT || 8080;
+
+//middleware setups
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static("website"));
+server.use(cors());
+
+//get call;
+projectData = {};
+
+server.get("/getData", (req, res) => {
+  res.send(projectData);
+});
+
+//post call
+server.post("/addData", (req, res) => {
+  const data = req.body;
+  projectData["temp"] = data.temp;
+  projectData["date"] = data.date;
+  projectData["feel"] = data.feel;
+});
+//get all
+server.get("/all", (req, res) => {
+  res.send(projectData);
+  console.log(projectData);
+});
+
+server.listen(port, () => console.log("serving weather journal"));
---
+++ website/app.js
@@ -0,0 +1,71 @@
+//const full_url = `https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}`;
+
+//apikey and base url
+const api_key = "&appid=eae4c7df9d0c80084e5da9ace7f1a8b9&units=emperial";
+const base_url = "https://api.openweathermap.org/data/2.5/weather?zip=";
+
+const generate = document.getElementById("generate");
+
+generate.addEventListener("click", performAction);
+
+//function to  generate data
+function performAction(e) {
+  e.preventDefault();
+  const feelings = document.getElementById("feelings").value;
+  const zip = document.getElementById("zip").value;
+  getData(base_url, zip, api_key)
+    .then(function (data) {
+      postData("/addData", {
+        temp: data.main.temp,
+        date: data.dt,
+        feel: feelings,
+      });
+    })
+    .then(retrieveData());
+}
+
+//get request
+const getData = async (url = "", zipcode = "", key = "") => {
+  const resp = await fetch(url + zipcode + key);
+  try {
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//post request
+const postData = async (url = "", data = {}) => {
+  const resp = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  try {
+    const newData = await resp.json();
+    return newData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// updating the user interface
+const retrieveData = async () => {
+  const request = await fetch("/all");
+  try {
+    const allData = await request.json();
+
+    console.log(allData);
+
+    document.getElementById("temp").innerHTML =
+      Math.round(allData.temp) + " degrees";
+    document.getElementById("content").innerHTML = allData.feel;
+    document.getElementById("date").innerHTML = new Date(
+      allData.date * 1000
+    ).toLocaleDateString();
+  } catch (error) {
+    console.log("error", error);
+  }
+};
